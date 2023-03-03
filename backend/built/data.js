b7ce53e95ab46853fe17466e15a0e355d7cdd1df
victorcoliban <64005698+victorcoliban@users.noreply.github.com>
2023-03-03 15:35:01 +0200

    publish to render.com

---
+++ backend/built/data.js
@@ -0,0 +1,165 @@
+"use strict";
+exports.__esModule = true;
+exports.sample_users = exports.sample_tags = exports.sample_foods = void 0;
+exports.sample_foods = [
+    {
+        id: '1',
+        name: 'Hummus',
+        price: 5.9,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/9699703.jpg?width=1200',
+        ingredients: "Garbanzo's homemade hummus recipe [vegan & gluten-free] topped with extra virgin olive oil, sumac & homemade za'atar. Served with pita bread.",
+        tags: ['Hummus']
+    },
+    {
+        id: '2',
+        name: 'Tabbouleh (vegan & gluten-free)',
+        price: 5.5,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/11263530.jpg?width=1200',
+        ingredients: 'Our gluten-free take on probably the most popular Levantine salad, Tabbouleh. Made with cauliflower rice, tomatoes, fresh parsley & mint, red onion, extra virgin olive oil and lemon juice.',
+        tags: ['Salads']
+    },
+    {
+        id: '3',
+        name: "Za'atar Fries",
+        price: 3.8,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/10977625.jpg?width=1200',
+        ingredients: "Fries tossed in Garbanzo's homemade mix of spices. Served optionally with a dip of your choice.",
+        tags: ['Fries']
+    },
+    {
+        id: '4',
+        name: 'Classic Falafel Wrap',
+        price: 7.9,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/305478.jpg?width=1200',
+        ingredients: '4x crispy falafel, homemade hummus, fresh tomatoes, cucumber & mint, white cabbage, fresh parsley & black cumin, sumac red onion, homemade pickled radish & homemade vegan mayonnaise all wrapped in freshly baked Lebanese flatbread.',
+        tags: ['Falafel', 'Wraps', 'Hummus']
+    },
+    {
+        id: '5',
+        name: 'Black Beans Falafel Wrap',
+        price: 7.9,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/305468.jpg?width=1200',
+        ingredients: '4x crispy black beans falafel, homemade hummus, fresh tomatoes, cucumber & mint, iceberg, sumac red onion, homemade pickled radish & zhoug sauce, all wrapped in freshly baked Lebanese flatbread.',
+        tags: ['Falafel', 'Wraps', 'Hummus']
+    },
+    {
+        id: '6',
+        name: 'Cauliflower Shawarma Wrap',
+        price: 7.9,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/305474.jpg?width=1200',
+        ingredients: '4x marinated and deep fried cauliflower shawarma, homemade hummus, fresh tomatoes, cucumber & mint, white cabbage, fresh parsley & black cumin,, sumac red onion, homemade pickled radish & homemade vegan mayonnaise all wrapped in freshly baked Lebanese flatbread.',
+        tags: ['Wraps', 'Hummus']
+    },
+    {
+        id: '7',
+        name: 'Halloumi & Couscous Wrap',
+        price: 7.9,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/305473.jpg?width=1200',
+        ingredients: '5x crispy Halloumi cheese fries, Moroccan couscous, homemade hummus, fresh tomato, cucumber and mint salad, white cabbage, fresh parsley & black cumin, homemade pickled radish & Amba sauce (pickled mango), all wrapped in freshly baked Lebanese bread.',
+        tags: ['Wraps', 'Hummus']
+    },
+    {
+        id: '8',
+        name: 'Falafel',
+        price: 5.9,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/9699702.jpg?width=1200',
+        ingredients: '5x homemade Falafel (vegan & gluten-free) served pita bread, and optionally a dip of your choice.',
+        tags: ['Falafel']
+    },
+    {
+        id: '9',
+        name: 'Vegan Shawarma',
+        price: 6.9,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/9754861.jpg?width=1200',
+        ingredients: "Deep fried cauliflower (vegan & gluten-free) marinated in Garbanzo's homemade shawarma mix of spice, served with pita bread.",
+        tags: ['Wraps']
+    },
+    {
+        id: '10',
+        name: 'Pita Chips',
+        price: 3.5,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/9754859.jpg?width=1200',
+        ingredients: "Deep fried chips made of pita bread and tossed with Garbanzo's homemade Za'atar.",
+        tags: ['Fries']
+    },
+    {
+        id: '11',
+        name: 'Halloumi Fries',
+        price: 8.5,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/9754860.jpg?width=1200',
+        ingredients: 'Deep fried Halloumi cheese topped with fresh mint, dried cranberries, homemade vegan mayo, pomegranate molasses & black cumin.',
+        tags: ['Fries']
+    },
+    {
+        id: '12',
+        name: 'Chips & Dips',
+        price: 7.9,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/11246251.jpg?width=1200',
+        ingredients: 'Homemade Za’atar Pita Chips and 4 of our Dips: homemade vegan mayo (soya free), zhoug (parsley, coriander, garlic), amba (pickled mango) and rose harissa [spicy].',
+        tags: ['Fries']
+    },
+    {
+        id: '13',
+        name: 'Fattoush',
+        price: 5.5,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/11278573.jpg?width=1200',
+        ingredients: 'Chunky tomatoes and cucumbers, fresh mint & crispy za’atar pita chips.',
+        tags: ['Salads']
+    },
+    {
+        id: '14',
+        name: 'Homemade Vegan Mayo',
+        price: 1.5,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/305529.jpg?width=1200',
+        ingredients: 'Homemade with aquafaba',
+        tags: ['Dips']
+    },
+    {
+        id: '15',
+        name: 'Zhoug Dip (vegan)',
+        price: 1.5,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/305531.jpg?width=1200',
+        ingredients: 'Made of fresh coriander & parsley, garlic and a touch of chilli.',
+        tags: ['Dips']
+    },
+    {
+        id: '16',
+        name: 'Amba Dip (vegan)',
+        price: 1.5,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/305533.jpg?width=1200',
+        ingredients: 'Pickled mango sauce.',
+        tags: ['Dips']
+    },
+    {
+        id: '17',
+        name: 'Rose Harissa [vegan]',
+        price: 1.5,
+        imageUrl: 'https://images.deliveryhero.io/image/fd-mj/Products/305524.jpg?width=1200',
+        ingredients: 'Spicy and fragrant.',
+        tags: ['Dips']
+    },
+];
+exports.sample_tags = [
+    { name: 'Falafel' },
+    { name: 'Wraps' },
+    { name: 'Hummus' },
+    { name: 'Fries' },
+    { name: 'Salads' },
+    { name: 'Dips' },
+];
+exports.sample_users = [
+    {
+        name: "Victor Coliban",
+        email: "victor@gmail.com",
+        password: "q1w2e3",
+        address: "Bucharest, Romania",
+        isAdmin: true
+    },
+    {
+        name: "Ion Tiriac",
+        email: "ion@gmail.com",
+        password: "q1w2e3",
+        address: "Texas, USA",
+        isAdmin: false
+    }
+];
